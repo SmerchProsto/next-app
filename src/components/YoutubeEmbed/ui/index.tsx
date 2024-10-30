554c20bab62e2b8144b5ad9d4cbab06f47b2f404
SmerchProsto <smerchprosto@gmail.com>
2024-10-31 02:14:45 +0400

    fix youtube link

--- src/components/YoutubeEmbed/ui/index.tsx
+++ src/components/YoutubeEmbed/ui/index.tsx
@@ -4,9 +4,12 @@ interface YoutubeEmbedProps extends React.IframeHTMLAttributes<HTMLIFrameElement
     videoUrl: string;
 }
 export default function YoutubeEmbed({ videoUrl, ...rest  }: YoutubeEmbedProps) {
+    // Извлекаем ID видео из URL и преобразуем в формат для embed
+    const videoId = new URL(videoUrl).searchParams.get("v");
+    const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
     return (
         <iframe
-            src={videoUrl}
+            src={embedUrl}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
