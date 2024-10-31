df6a23c8370033cb85763729775153079329893d
SmerchProsto <smerchprosto@gmail.com>
2024-10-31 06:50:22 +0400

    all is complete

--- README.md
+++ README.md
@@ -26,11 +26,11 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 5. All pages are designed according to generally accepted SEO and accessibility recommendations
 6. Form fields must be configured using CSS
 7. YouTube videos are loaded using the react-lazy-load library
+8. Added media preview Open Graph, twitter cards by meta tags
 ##   Important additions:
 1. Semantics: the code is written taking into account the semantic design, hierarchy of headings, tag structure
 2. Styling by classes: For better readability of names stylized elements, I implemented a partially similar spelling by class names as in BEM. (element__element-component = HeaderNavigation, HeaderLogo, etc.). I also added a reusable SectionInfoBlock class for example. Also, with good UI/UX, I added a transition to the main page when clicking on the company logo (some company in the header), if the user is not on it
 3. The structure of the project directories: under the conditions of quickly writing a test task, a modular structure was implemented + a similar template for the design of segment names as FSD
-
 ## To sum up
 We have an application that is adaptive for mobile and desktop devices, with the highest possible readability of the code and its design. The application is configured to interact with the server and, with further expansion, is able to communicate with it through other endpoints.
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,30 @@ import LessInfoSection from "@/components/Index/LessInfoSection/ui";
 export default function Index() {
   return (
     <>
-      <Head>
-          <meta name='title' content='Main Page' />
-          <meta name='description' content='app main page with content' />
-          <meta name='keywords' content='app main, app home' />
-          <title>Main Page</title>
-      </Head>
+        <Head>
+            <meta name='title' content='Main Page'/>
+            <meta name='description' content='app main page with content'/>
+            <meta name='keywords' content='app main, app home'/>
+            <title>Main Page</title>
+
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="http://localhost:3000/contact"/>
+            <meta property="og:title" content="Main page"/>
+            <meta property="og:description" content="Information about us."/>
+            <meta property="og:image" content="/Rick-Astley.webp"/>
+
+
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:url" content="http://localhost:3000/contact"/>
+            <meta name="twitter:title" content="Contact Us"/>
+            <meta name="twitter:description" content="Information about us."/>
+            <meta name="twitter:image" content="/Rick-Astley.webp"/>
+        </Head>
         <Main elements={[
             <InfoSection key='infoSection'/>,
             <Features key='Features'/>,
             <LessInfoSection key='LessInfoSection'/>
-        ]} />
+        ]}/>
     </>
   );
 }
